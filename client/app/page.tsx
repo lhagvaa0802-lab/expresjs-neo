7850a4e22e11a4820e8fdcb6c9996ec97a0cd6f3
lhagvaa0802-lab <lhagvaa0802@gmail.com>
2026-03-20 12:04:37 +0800

    draft

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
-  );
+export default async function Home() {
+  const data = await fetch("http://localhost:3001/users", {
+    method: "GET",
+    headers: {
+      authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImVtYWlsIjoibGhhZ3ZhYUBleGFtcGxlLmNvbSIsInJvbGUiOiJBRE1JTiIsImlhdCI6MTc3Mzk3ODE4OSwiZXhwIjoxNzczOTgxNzg5fQ.fN-whYTLGATyd00zpa63SBd7oP8u3nVMNDV1nBuraMU`,
+    },
+  });
+  const users = await data.json();
+  console.log(users);
+  return <div>"ok"</div>;
 }
